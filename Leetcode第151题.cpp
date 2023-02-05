ffd3d2807747ad7d17758bde2ac1e0c4eec02eb7
ImDebuging <110248244+ImDebuging@users.noreply.github.com>
2023-02-05 20:45:55 +0800

    Create Leetcode第151题.cpp

---
+++ Leetcode第151题.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void reverse(string &s,int start,int end)
+    {
+        for (int i = start, j = end; i < j; i++, j--)   {
+            swap(s[i], s[j]);
+        }
+    }
+    void removeExtraSpaces(string &s)
+    {
+        int slow=0;
+        for(int i=0;i<s.size();++i)
+        {
+            if(s[i]!=' '){
+                if(slow>0){s[slow++]=' ';}
+                while(i<s.size()&&s[i]!=' ')
+                {
+                    s[slow++]=s[i++];
+                }
+            }
+        }
+        s.resize(slow);
+    }
+    string reverseWords(string s) {
+        removeExtraSpaces(s);
+        reverse(s,0,s.size()-1);
+        for(int i=0,j=0;i<=s.size();++i)
+        {
+            if(s[i]==' '||i==s.size())
+            {
+                reverse(s,j,i-1);
+                j=i+1;
+            }
+        }
+        return s;
+    }
+};
